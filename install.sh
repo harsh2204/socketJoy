ec687ea81dc16ee59e3eddd9ee3f88a6bc93a9d4
harsh2204 <harshgupta2204@live.com>
2020-12-06 07:06:50 +0000

    Deploying to gh-pages from  @ 15db5ca3e67cd9d5ee8db0e518a4e66cd1ba52ad 🚀

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ mkdir socket-joy
 git clone https://github.com/harsh2204/Joy2DroidX-server.git j2dx-server/
 git clone https://github.com/harsh2204/socketjoy.git socket-joy/
 
+cd socketjoy
 ./socket-joy/run.sh
