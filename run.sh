4a47e402fa1357449c73d9fcdaae9143caa3ae83
harsh2204 <harshgupta2204@live.com>
2020-12-12 14:23:43 +0000

    Deploying to gh-pages from  @ 40585062cdfb2eaa3a87e9aa3541c395c2798716 🚀

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 which deactivate
 rv=$?
-if [ $rv-eq 0 ]; then
+if [ $rv -eq 0 ]; then
     echo "venv already sourced"
 else
-    ../bin/activate
+    source ../bin/activate
 fi
 
 j2dx & python3 -m http.server && kill $! && deactivate
