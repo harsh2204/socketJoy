3822ea2b7b13c6470ea5ef1714792ba306726e88
harsh2204 <harshgupta2204@live.com>
2020-12-16 02:50:14 +0000

    Deploying to gh-pages from  @ d5efb92f3deaf92e152270950d60bee55ccb51e8 🚀

--- run.sh
+++ run.sh
@@ -1,10 +1,30 @@
-#! /usr/bin/env bash
-which deactivate
-rv=$?
-if [ $rv -eq 0 ]; then
-    echo "venv already sourced"
+#!/bin/sh
+function usage() {
+    echo "Usage: ./run.sh [virtual-env-directory]"
+    echo
+}
+function error() {
+    echo "Something went wrong when sourcing the venv. Please check your venv directory and ensure j2dx is installed correctly."
+    usage
+    exit 1
+}
+
+type -t j2dx >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "j2dx found! running instance"
 else
-    source ../bin/activate
+    if [ "$#" -eq 1 ]; then
+        source $1/bin/activate || error
+
+        type -t j2dx >/dev/null 2>&1 || error
+        
+    elif [ "$#" -eq 0 ]; then
+        echo "Sourcing default directory from ../bin/activate"
+        source ../bin/activate
+    else
+        usage
+        exit 1
+    fi
 fi
 
-j2dx & python3 -m http.server && kill $! && deactivate
+j2dx & python3 -m http.server && kill $!
